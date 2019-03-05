67760be580617fc304d1502f09b82d4d05391022
hvate <Itsposs@163.com>
2019-03-05 11:22:19 +0800

    190301

--- outline/template.c
+++
@@ -1,7 +0,0 @@
-#include "apue.h"
-
-
-int main(int argc,char *argv[])
-{
-	return 0;
-}
--- pthread/template.c
+++
@@ -1,8 +0,0 @@
-#include "apue.h"
-#include <pthread.h>
-
-
-int main(int argc,char *argv[])
-{
-	exit(0);
-}
